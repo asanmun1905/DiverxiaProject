afbc2c745cc9e7f2456e37ffeb73e590c39ad9a0
asanmun1905 <adri1952005.4@gmail.com>
2026-06-03 18:46:45 +0200

    update supabase configuration keys on disk

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -1,6 +1,6 @@
 
-const SUPABASE_URL = 'https://voalryklgwuphgfwvobl.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_juM4FPHYg7l5GagtyM91sQ_xIPuydHC';
+const SUPABASE_URL = 'https://tmosvjkqodrnqopoydwp.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_lZ0sSXcQZ-seKkuWb-gABQ_jZgyCoAj';
  
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
@@ -38,4 +38,4 @@ export async function fetchUserMap() {
     }
     
     return userMap;
-}
+}
